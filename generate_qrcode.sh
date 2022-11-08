5844ba8d6ad5e13f8f649e2b6ca12acd36029eda
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-11-08 00:30:22 -0500

    Update generate_qrcode.sh

--- generate_qrcode.sh
+++ generate_qrcode.sh
@@ -1,4 +1,9 @@
-qrencode -o test.png  "https://github.com"
+###############################################
+# generate a qrcode and see it using command line 
+#=============================================
+
+# generate a qrcode whose size is 10, output filename is test.png, and text is "https://github.com"
+qrencode -s 10 -o test.png  "https://github.com"
 
 # vieiw the qrcode with file manager
 nautilus # already installed in debian
