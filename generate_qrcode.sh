5c2da2ae9fd8c6c7ba8752e2538f0a2349b70cc2
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-11-08 00:24:41 -0500

    Create generate_qrcode.sh

---
+++ generate_qrcode.sh
@@ -0,0 +1,9 @@
+qrencode -o test.png  "https://github.com"
+
+# vieiw the qrcode with file manager
+nautilus # already installed in debian
+
+# vieiw the qrcode with display command
+display test.png
+
+
