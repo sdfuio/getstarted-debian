a16d193e2963badbcdee51469e2209a072ac7187
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-12-08 11:13:24 +0000

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,12 @@
+wget https://www.torproject.org/dist/torbrowser/12.0/tor-browser-linux64-12.0_ALL.tar.xz
+wget https://www.torproject.org/dist/torbrowser/12.0/tor-browser-linux64-12.0_ALL.tar.xz.asc
+
+wget https://download3.vmware.com/software/WKST-PLAYER-1700/VMware-Player-Full-17.0.0-20800274.x86_64.bundle
+# MD5SUM:	5192290e1a18eb66ac581a41bb337a53
+# SHA1SUM: 	737621e01ee7c9460ca9567970f99d2e79fbce9f
+# SHA256SUM: 	97080724e607b98887962b012283c481484146b4303c4897387dd86ce3a9aca1
+# https://www.vmware.com/products/workstation-player.html
+
+
+
+
