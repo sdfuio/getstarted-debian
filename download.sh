2979a3e4fcd8ec5a8f77b41379e2f7f261668d44
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-12-08 21:05:29 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -18,3 +18,13 @@ https://download.tails.net/tails/stable/tails-amd64-5.7/tails-amd64-5.7.iso
 https://tails.boum.org/torrents/files/tails-amd64-5.7.iso.sig
 https://tails.boum.org/tails-signing.key
 
+
+
+
+https://bunny.deb.parrot.sh//parrot/iso/5.1.2/Parrot-home-5.1.2_amd64.iso
+# 5fe197d3381a4cc816eb523c32532076
+# b04b4ea5bcbff79d5a098cd6524296a37d2f7ee7
+# b907e0dc4ba156566471a8a271ddd575a7af2c5af4cc8bc18a949cc3
+# 8ed307e7a135ee3bca048abf609ed8af22ecae5fb0e1c72472d39929c7cf827a
+# 34e4171853fe502b3130d0c82da5af9fd15b79b910fe9a03c83aa522eea1042d40e1ca10e294259584ab5467b8be5e33
+# 98001b8c49f2eeb44cb354b630a43681c2c1bb6e6ad15e3c0d70fdcf8e5f85d918cd9b8afa91f546b7f93d158e98858722db17e2b7e209321dc93d2980e05cb8
