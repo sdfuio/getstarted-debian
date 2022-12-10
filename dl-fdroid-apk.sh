0f5dd3234989a2f94804d015c9912bbc7e329e32
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-12-10 15:35:27 +0900

    Create dl-fdroid-apk.sh

---
+++ dl-fdroid-apk.sh
@@ -0,0 +1,35 @@
+
+
+wget https://f-droid.org/repo/org.osmdroid_50.apk
+wget https://f-droid.org/repo/org.osmdroid_50.apk.asc
+
+
+
+
+
+# verify
+gpg --verify org.osmdroid_50.apk.asc 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
