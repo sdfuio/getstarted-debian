10cc9c018284be49b95d87e0a927935141a9362f
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-12-26 00:33:07 +0900

    Create install-protonvpn.sh

--- install-protonvpn.sh
+++ install-protonvpn.sh
@@ -1,5 +1,6 @@
 
 # https://protonvpn.com/support/official-linux-vpn-mint/
+sudo apt install ca-certificates wget curl
 
 wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb
 sudo dpkg -i protonvpn-stable-release_1.0.3_all.deb
